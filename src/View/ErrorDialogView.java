d31bb1eadd2c0c2c50e0b07484049a8b640d95d6
Zied Thabet <zied.thabet1@gmail.com>
2014-11-11 23:47:13 +0100

    Validate Node ID unicity

--- src/View/ErrorDialogView.java
+++ src/View/ErrorDialogView.java
@@ -6,10 +6,19 @@ import javax.swing.JOptionPane;
 public class ErrorDialogView {
 	
 	public void paint(Exception ex){
-		JOptionPane.showMessageDialog(new JFrame(),
-			    ex.getMessage().split(":")[1],
-			    ex.getClass().getSimpleName(),
-			    JOptionPane.ERROR_MESSAGE);
+		try{
+			JOptionPane.showMessageDialog(new JFrame(),
+				    ex.getMessage().split(":")[1],
+				    ex.getClass().getSimpleName(),
+				    JOptionPane.ERROR_MESSAGE);
+		} catch (ArrayIndexOutOfBoundsException eob){
+			JOptionPane.showMessageDialog(new JFrame(),
+				    ex.getMessage(),
+				    ex.getClass().getSimpleName(),
+				    JOptionPane.ERROR_MESSAGE);
+		}
+		
+		
 	}
 	
 	
